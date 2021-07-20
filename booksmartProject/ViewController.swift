5dd0d4ca3d7a5242254dfe193f1c9625580f9017
Amanda Rajkovich <amanda.rajkovich@students.loy.org>
2021-07-20 14:03:12 -0500

    added a "button"

---
+++ booksmartProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  booksmartProject
+//
+//  Created by Amanda on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+//I added a button
+
+}
+
